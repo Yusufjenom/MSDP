761c49801fd93db9064840cddbba10c083dc60a4
Yusufjenom <jenomy32@gmail.com>
2024-12-30 14:09:34 +0100

    update

---
+++ utils/tryCatch.js
@@ -0,0 +1,9 @@
+const TryCatchFn = (controller) => async (req, res, next) => {
+  try {
+    await controller(req, res);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.export = { TryCatchFn };
